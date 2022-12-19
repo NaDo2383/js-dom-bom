f769ecec9009965a54d5a35e7008d5566e946409
NaDo2383 <narandorj.byambadorj@gmail.com>
2022-12-19 12:34:42 +0800

    20221219 class

---
+++ aimagsum/main.js
@@ -0,0 +1,24 @@
+let jagsaalt_sum = [
+    { aimag: "Архангай", sum: "Жаргалант сум" },
+    { aimag: "Архангай", sum: "Цахир сум" },
+    { aimag: "Архангай", sum: "Чулуут сум" },
+    { aimag: "Баянхонгор", sum: "Галуут сум" },
+    { aimag: "Баянхонгор", sum: "Жаргалант сум" },
+    { aimag: "Баянхонгор", sum: "Баянговь сум" },
+    { aimag: "Булган", sum: "Рашаант сум" },
+    { aimag: "Булган", sum: "Сэлэнгэ сум" },
+    { aimag: "Булган", sum: "Хангал сум" },
+    { aimag: "Говь-Алтай", sum: "Төгрөг сум" },
+    { aimag: "Говь-Алтай", sum: "Халиун сум" },
+    { aimag: "Говь-Алтай", sum: "Бигэр сум" },
+    { aimag: "Говьсүмбэр", sum: "Шивээговь сум" },
+    { aimag: "Говьсүмбэр", sum: "Баянтал сум" },
+]
+
+let jagsaalt_aimag = ["Архангай", "Баянхонгор", "Булган", "Говь-Алтай", "Говьсүмбэр"];
+let aimag = document.getElementById("aimag");
+let sum = document.getElementById("sum");
+function makingAimagList() {
+
+
+}
